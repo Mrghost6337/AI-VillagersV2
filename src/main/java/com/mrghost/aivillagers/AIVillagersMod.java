09785984f345dcc9ae3e25448207eb3dd393274a
Mrghost <snelle.edward@gmail.com>
2026-01-21 14:51:25 +0100

    Create AIVillagersMod.java

---
+++ src/main/java/com/mrghost/aivillagers/AIVillagersMod.java
@@ -0,0 +1,13 @@
+package com.mrghost.aivillagers;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AIVillagersMod implements ModInitializer {
+
+    public static final String MOD_ID = "aivillagers";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("AI Villagers mod loaded");
+    }
+}
