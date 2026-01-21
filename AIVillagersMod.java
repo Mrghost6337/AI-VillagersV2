0fe318167ad6849026f013c046235c79c5c8e412
Mrghost <snelle.edward@gmail.com>
2026-01-21 14:48:45 +0100

    Add files via upload

---
+++ AIVillagersMod.java
@@ -0,0 +1,11 @@
+
+package com.aivillagers;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AIVillagersMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("[AI Villagers] Fabric 1.21.10 loaded");
+    }
+}
